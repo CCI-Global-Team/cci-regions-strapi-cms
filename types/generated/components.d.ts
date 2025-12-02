c9321e640812e61980148edaaf160695ee23fc29
Fiyin Adebayo <fiyinsimeon@gmail.com>
2025-12-01 18:41:49 -0800

    feat: start building navbar and homepage structure

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,45 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface GlobalNavItem extends Struct.ComponentSchema {
+  collectionName: 'components_global_nav_items';
+  info: {
+    displayName: 'NavItem';
+    icon: 'layout';
+  };
+  attributes: {
+    label: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.Unique;
+    order: Schema.Attribute.Integer & Schema.Attribute.Required;
+    target: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'_self'>;
+    url: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface GlobalNavLogo extends Struct.ComponentSchema {
+  collectionName: 'components_global_nav_logos';
+  info: {
+    displayName: 'NavLogo';
+    icon: 'layout';
+  };
+  attributes: {
+    alt: Schema.Attribute.String;
+    height: Schema.Attribute.Integer;
+    label: Schema.Attribute.String & Schema.Attribute.Required;
+    logo: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    target: Schema.Attribute.String & Schema.Attribute.DefaultTo<'_self'>;
+    url: Schema.Attribute.String & Schema.Attribute.Required;
+    width: Schema.Attribute.Integer;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'global.nav-item': GlobalNavItem;
+      'global.nav-logo': GlobalNavLogo;
+    }
+  }
+}
