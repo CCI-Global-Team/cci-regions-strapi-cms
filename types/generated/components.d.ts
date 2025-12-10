3a4ea5223e960d3444fe128cbb02a6df4c705809
Fiyin Adebayo <fiyinsimeon@gmail.com>
2025-12-10 12:12:12 -0800

    Initial Structure (#1)
    * feat: start building navbar and homepage structure
    
    * Add button and hero components, start building homepage

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,89 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface GlobalButton extends Struct.ComponentSchema {
+  collectionName: 'components_global_buttons';
+  info: {
+    displayName: 'Button';
+    icon: 'bold';
+  };
+  attributes: {
+    classNames: Schema.Attribute.String;
+    icon: Schema.Attribute.Media<'images'>;
+    iconText: Schema.Attribute.String;
+    label: Schema.Attribute.String & Schema.Attribute.Required;
+    order: Schema.Attribute.Integer;
+    url: Schema.Attribute.String;
+    variant: Schema.Attribute.Enumeration<
+      [
+        'primary',
+        'secondary',
+        'tertiary',
+        'outline',
+        'error',
+        'destructive',
+        'success',
+      ]
+    > &
+      Schema.Attribute.Required;
+  };
+}
+
+export interface GlobalNavItem extends Struct.ComponentSchema {
+  collectionName: 'components_global_nav_items';
+  info: {
+    displayName: 'NavItem';
+    icon: 'layout';
+  };
+  attributes: {
+    label: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.Unique;
+    order: Schema.Attribute.Integer & Schema.Attribute.Required;
+    target: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'_self'>;
+    url: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface GlobalNavLogo extends Struct.ComponentSchema {
+  collectionName: 'components_global_nav_logos';
+  info: {
+    displayName: 'NavLogo';
+    icon: 'layout';
+  };
+  attributes: {
+    alt: Schema.Attribute.String;
+    height: Schema.Attribute.Integer;
+    label: Schema.Attribute.String & Schema.Attribute.Required;
+    logo: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    target: Schema.Attribute.String & Schema.Attribute.DefaultTo<'_self'>;
+    url: Schema.Attribute.String & Schema.Attribute.Required;
+    width: Schema.Attribute.Integer;
+  };
+}
+
+export interface SectionHero extends Struct.ComponentSchema {
+  collectionName: 'components_section_heroes';
+  info: {
+    displayName: 'Hero';
+    icon: 'apps';
+  };
+  attributes: {
+    actions: Schema.Attribute.Component<'global.button', true>;
+    background: Schema.Attribute.Media<'images' | 'videos'>;
+    subtitle: Schema.Attribute.String;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'global.button': GlobalButton;
+      'global.nav-item': GlobalNavItem;
+      'global.nav-logo': GlobalNavLogo;
+      'section.hero': SectionHero;
+    }
+  }
+}
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -430,6 +430,83 @@ export interface AdminUser extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiHomePageHomePage extends Struct.SingleTypeSchema {
+  collectionName: 'home_pages';
+  info: {
+    displayName: 'HomePage';
+    pluralName: 'home-pages';
+    singularName: 'home-page';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  pluginOptions: {
+    i18n: {
+      localized: true;
+    };
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    header: Schema.Attribute.Component<'section.hero', false> &
+      Schema.Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
+    locale: Schema.Attribute.String;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::home-page.home-page'
+    >;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
+export interface ApiNavbarNavbar extends Struct.SingleTypeSchema {
+  collectionName: 'navbars';
+  info: {
+    displayName: 'Navbar';
+    pluralName: 'navbars';
+    singularName: 'navbar';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  pluginOptions: {
+    i18n: {
+      localized: true;
+    };
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    items: Schema.Attribute.Component<'global.nav-item', true> &
+      Schema.Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
+    locale: Schema.Attribute.String;
+    localizations: Schema.Attribute.Relation<'oneToMany', 'api::navbar.navbar'>;
+    logos: Schema.Attribute.Component<'global.nav-logo', true> &
+      Schema.Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -940,6 +1017,8 @@ declare module '@strapi/strapi' {
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
       'admin::user': AdminUser;
+      'api::home-page.home-page': ApiHomePageHomePage;
+      'api::navbar.navbar': ApiNavbarNavbar;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
