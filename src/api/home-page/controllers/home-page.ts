d40ef47f8625800daf78f98011acc632ed120e32
Fiyin Adebayo <fiyinsimeon@gmail.com>
2025-12-21 19:08:36 -0800

    Write custom code query to append upcoming events to home-page response

--- src/api/home-page/controllers/home-page.ts
+++ src/api/home-page/controllers/home-page.ts
@@ -3,5 +3,28 @@
  */
 
 import { factories } from '@strapi/strapi'
+import type { Context } from 'koa';
 
-export default factories.createCoreController('api::home-page.home-page');
+export default factories.createCoreController('api::home-page.home-page', ({ strapi }) => ({
+  async find(ctx: Context) {
+    const response = await super.find(ctx);
+
+    const events = await strapi.entityService.findMany('api::event.event', {
+      sort: { createdAt: 'desc' },
+      limit: 3,
+      populate: '*',
+    });
+
+    return {
+      ...response,
+      data: {
+        ...response.data,
+        upcomingEvents: {
+          title: 'Upcoming Events',
+          viewAll: 'View all events',
+          events
+        },
+      },
+    };
+  },
+}));
